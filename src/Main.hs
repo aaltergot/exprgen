bd70db9b10f5ded0800a39eca9717e9f20321d5b
aaltergot <avaltergot@gmail.com>
2016-04-09 11:45:11 +0600

    exprt dsl

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,26 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+
+data Op = A -- Add
+        | M -- Multiply
+        | D -- Divide
+        deriving (Show, Eq)
+
+succOp :: Op -> Op
+succOp A = M
+succOp M = D
+succOp _ = A
+
+succOps :: [Op] -> [Op]
+succOps [] = []
+succOps (op:ops) = let next = succOp op
+                    in next:(if next == A then succOps ops else ops)
+
+data Expr a = Val a 
+            | Expr (Expr a) (Expr a) deriving (Show)
+
+nodeCnt :: Expr a -> Int
+nodeCnt (Expr l r)  = 1 + nodeCnt l + nodeCnt r
+nodeCnt _ = 0
